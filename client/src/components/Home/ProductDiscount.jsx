6a261625d7838bb9dfbeb95541c9c6557e3a0e8d
hoangtuandev <89229844+hoangtuandev@users.noreply.github.com>
2022-03-14 09:44:22 +0700

    Update ProductDiscount.jsx

--- client/src/components/Home/ProductDiscount.jsx
+++ client/src/components/Home/ProductDiscount.jsx
@@ -1,99 +1,48 @@
-// import React from 'react';
-// import axios from 'axios'
-// import { Link } from 'react-router-dom'
-// import styles from './Home.module.scss'
-
-// function ProductDiscount(props) {
-
-//     const { data, handleGetProductSelected } = props
-
-//     const baseURL = 'http://localhost:3333'
-//     const imgProduct = require(`../../images/${data.sp_hinhanh}`)
-//     const priceNew = data.sp_gia - (data.sp_gia * (data.spkm_tylegiam / 100))
-
-//     const handleSelectProduct = () => {
-//         axios.post(baseURL + '/products/ProductById', { id: data.sp_id })
-//             .then((res) => {
-//                 // console.log(res.data[res.data.length - 1])
-//                 handleGetProductSelected(res.data[res.data.length - 1])
-//             })
-//     }
-
-//     return (
-//         <Link to={`/products/details`} className={`${styles.linkRoute}`}>
-//             <li onClick={handleSelectProduct}>
-//                 <div className={`${styles.main}`}>
-//                     <div className={`${styles.productImage}`}>
-//                         <img src={imgProduct} alt="" />
-//                     </div>
-//                     <div className={`${styles.productRatio}`}>{data.spkm_tylegiam}%</div>
-//                     <div className={`${styles.productName}`}>{data.sp_tensanpham}</div>
-//                     <div className={`${styles.productPriceNew}`}>
-//                         <sup className={`${styles.priceUnit}`}>đ</sup>
-//                         {Math.floor(priceNew / 1000)}
-//                         <span className={`${styles.priceThounsand}`}>.000</span>
-
-//                     </div>
-//                     <del className={`${styles.productPriceOld}`}>
-//                         {data.sp_gia / 1000}.000 <sup>đ</sup>
-//                     </del>
-//                     <div className={`${styles.productButton}`}>
-//                         <button>Mua Ngay</button>
-//                     </div>
-//                 </div>
-//             </li>
-//         </Link>
-
-//     );
-// }
-
-// export default ProductDiscount;
-
-
 import React from 'react';
 import axios from 'axios'
-import { useCookies } from 'react-cookie';
+import { Link } from 'react-router-dom'
 import styles from './Home.module.scss'
 
 function ProductDiscount(props) {
 
-    const { data } = props
+    const { data, handleGetProductSelected } = props
 
     const baseURL = 'http://localhost:3333'
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
     const imgProduct = require(`../../images/${data.sp_hinhanh}`)
     const priceNew = data.sp_gia - (data.sp_gia * (data.spkm_tylegiam / 100))
 
-    const handleSelectProduct = (data) => {
-        removeCookie('idProduct')
-        setCookie('idProduct', data.sp_id, { path: '/' })
-        window.location.href = '/products/details'
+    const handleSelectProduct = () => {
+        axios.post(baseURL + '/products/ProductById', { id: data.sp_id })
+            .then((res) => {
+                // console.log(res.data[res.data.length - 1])
+                handleGetProductSelected(res.data[res.data.length - 1])
+            })
     }
 
     return (
-        // <Link to={`/products/details`} className={`${styles.linkRoute}`}>
-        <li onClick={() => handleSelectProduct(data)}>
-            <div className={`${styles.main}`}>
-                <div className={`${styles.productImage}`}>
-                    <img src={imgProduct} alt="" />
-                </div>
-                <div className={`${styles.productRatio}`}>{data.spkm_tylegiam}%</div>
-                <div className={`${styles.productName}`}>{data.sp_tensanpham}</div>
-                <div className={`${styles.productPriceNew}`}>
-                    <sup className={`${styles.priceUnit}`}>đ</sup>
-                    {Math.floor(priceNew / 1000)}
-                    <span className={`${styles.priceThounsand}`}>.000</span>
+        <Link to={`/products/details`} className={`${styles.linkRoute}`}>
+            <li onClick={handleSelectProduct}>
+                <div className={`${styles.main}`}>
+                    <div className={`${styles.productImage}`}>
+                        <img src={imgProduct} alt="" />
+                    </div>
+                    <div className={`${styles.productRatio}`}>{data.spkm_tylegiam}%</div>
+                    <div className={`${styles.productName}`}>{data.sp_tensanpham}</div>
+                    <div className={`${styles.productPriceNew}`}>
+                        <sup className={`${styles.priceUnit}`}>đ</sup>
+                        {Math.floor(priceNew / 1000)}
+                        <span className={`${styles.priceThounsand}`}>.000</span>
 
+                    </div>
+                    <del className={`${styles.productPriceOld}`}>
+                        {data.sp_gia / 1000}.000 <sup>đ</sup>
+                    </del>
+                    <div className={`${styles.productButton}`}>
+                        <button>Mua Ngay</button>
+                    </div>
                 </div>
-                <del className={`${styles.productPriceOld}`}>
-                    {data.sp_gia / 1000}.000 <sup>đ</sup>
-                </del>
-                <div className={`${styles.productButton}`}>
-                    <button>Mua Ngay</button>
-                </div>
-            </div>
-        </li>
-        // </Link>
+            </li>
+        </Link>
 
     );
 }
